b1a1262918b293180594d4110add12ec7c392846
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-06-25 11:43:08 -0400

    initial push

--- MEALBURNER/MEALBURNER/settings.py
+++ MEALBURNER/MEALBURNER/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mealburner_app.apps.MealBurnerAppConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
