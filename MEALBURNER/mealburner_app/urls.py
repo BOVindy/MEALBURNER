76853c3eef8b657cba3d2a537d762848360d8320
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-06-25 14:37:00 -0400

    jacob push -first draft of models'

--- MEALBURNER/mealburner_app/urls.py
+++ MEALBURNER/mealburner_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index
 #from . import views
+
 urlpatterns = [
     # views.index
     path("", index, name="home")
--- MEALBURNER/mealburner_app/views.py
+++ MEALBURNER/mealburner_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'mealburner_app/home.html')
 # Create your views here.
