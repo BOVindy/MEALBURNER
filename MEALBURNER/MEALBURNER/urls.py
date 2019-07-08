a1c1e254c3824b44351efa58ad4166f5e7af1b53
BOVindy <benvierhile@gmail.com>
2019-07-08 16:44:10 -0400

    login and lots of styling

--- MEALBURNER/MEALBURNER/urls.py
+++ MEALBURNER/MEALBURNER/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views
+
+from .login_form import LoginForm
 
 urlpatterns = [
     path("", include("mealburner_app.urls")),
     path('admin/', admin.site.urls),
-    path('mealburner_app/', include('django.contrib.auth.urls')),
+    path('mealburner_app/', views.LoginView.as_view(), {'LoginForm':'../static/registration/login.html'}, name='login'),
     path('home/', TemplateView.as_view(template_name='home.html'))
 
 ]
